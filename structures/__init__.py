4f46bc2bd2bdb8ebadd2c3bfeb2dd1921f095010
jpmock98 <59740367+jpmock98@users.noreply.github.com>
2020-01-23 04:49:07 -0600

    Update __init__.py

--- structures/__init__.py
+++ structures/__init__.py
@@ -1,11 +1,11 @@
 """
-Information Getter Functions
+Information Retrieval Functions
 ~~~~~~~~~~~~~~~~~~~
 
 Use of MongoDB for data retrieval.
 """
 from .dt_struc import *
 
-__title__ = 'Information Getter Functions'
+__title__ = 'Information Retrieval Functions'
 __author__ = 'John Mock'
 __version__ = '0.0.1'
